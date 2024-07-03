3c03e07b8e6799a06a7a63d94324e81527b27283
Thanhntmany-ubuntu <thanhntmany@gmail.com>
2024-07-03 20:55:41 +0700

    ket hop mysql va expressjs

--- index.js
+++ index.js
@@ -6,6 +6,53 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/a', (req, res) => {
+  res.send('AAAAAÂ')
+})
+
+app.get('/a/b', (req, res) => {
+  const mysql = require("mysql2");
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root_password',
+  });
+  connection.execute(
+    'SHOW DATABASES;',
+    [],
+    function (err, results, fields) {
+      res.send(results)
+
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
+
+})
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root_password',
+});
+
+app.get("/tailieuhoctap", function (req, res) {
+
+  connection.execute(
+    'SHOW DATABASES;',
+    [],
+    function (err, results, fields) {
+
+      res.send(results)
+
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
